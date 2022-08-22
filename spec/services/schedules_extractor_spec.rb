126bb7f12245eb01cbeef58f2bb251ec9ebb899b
Luan Santos <luan@tootz.com.br>
2022-08-22 06:32:22 -0300

    improvement: add spec to when api returns an empty list

--- spec/services/schedules_extractor_spec.rb
+++ spec/services/schedules_extractor_spec.rb
@@ -13,7 +13,20 @@ RSpec.describe SchedulesExtractor, type: :model do
       let(:schedules_extractor) { SchedulesExtractor.new.call }
 
       it "should return a message" do
-        expect(schedules_extractor).to  eq("API is unavailable")
+        expect(schedules_extractor).to eq("API is unavailable")
+      end
+    end
+    
+    describe 'when api returns an empty list' do
+      before do
+        stub_request(:get, "https://aplicacao7.tst.jus.br/pautaws/rest/pautas").
+          with(
+            headers: { 'Connection'=>'close', 'Host'=>'aplicacao7.tst.jus.br', 'User-Agent'=>'http.rb/5.1.0' }
+          ).to_return(status: 200, body: [], headers: {})
+      end
+
+      it "no schedules should be created" do
+        expect(Schedule.count).to eq(0)
       end
     end
   end
