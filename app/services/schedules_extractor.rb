7b7b9fc036495a4e57bae759bb1081990147c341
Luan Santos <luan@tootz.com.br>
2022-08-22 05:39:51 -0300

    feat: schedules extractor

--- app/services/schedules_extractor.rb
+++ app/services/schedules_extractor.rb
@@ -8,7 +8,33 @@ class SchedulesExtractor
   end
 
   def call
-    response = HTTP.get(API_URL)
-    schedules = response.parse
+    schedules_from_api = HTTP.get(API_URL).parse
+
+    schedules_from_api.each do |schedule_from_api|
+      schedule_id_params = {
+        adjudicating_part_code: schedule_from_api['orgaoJudicante']['codOrgaoJudicante'],
+        year: schedule_from_api['anoPauta'],
+        number: schedule_from_api['numPauta'],
+        kind: schedule_from_api['tipSessao'],
+      }
+
+      if Schedule.where(schedule_id_params).count > 0
+        puts "Schedule #{schedule_id_params.map { |key, value| value }.join('-')} already extracted"
+      else
+        schedule_params = {
+          adjudicating_part_code: schedule_from_api['orgaoJudicante']['codOrgaoJudicante'],
+          kind: schedule_from_api['tipSessao'],
+          number: schedule_from_api['numPauta'],
+          year: schedule_from_api['anoPauta'],
+          date: schedule_from_api['dtaSessao'],
+          meta: schedule_from_api,
+          mode: schedule_from_api['codMeioJulgamento'],
+          published_at: schedule_from_api['dtaPublicacao'],
+        }
+        
+        Schedule.create(schedule_params)
+        TrialsExtractor.new(*schedule_id_params.values).call
+      end
+    end
   end
 end
