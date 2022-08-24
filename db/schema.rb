1237d2055bac842b8d8f7eb415e874a5c7e139f6
Luan Santos <luan@tootz.com.br>
2022-08-24 13:48:31 -0300

    feat: schedule model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_08_24_164411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "schedules", force: :cascade do |t|
+    t.integer "adjudicating_part_code"
+    t.string "number"
+    t.integer "year"
+    t.string "kind"
+    t.datetime "date"
+    t.datetime "published_at"
+    t.string "mode"
+    t.json "meta"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
