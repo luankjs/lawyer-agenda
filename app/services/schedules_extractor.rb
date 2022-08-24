56ada6a3c1864873f9d6e1484abffa87b2294535
Luan Santos <luan@tootz.com.br>
2022-08-24 16:44:09 -0300

    wip: get schedules from api

--- app/services/schedules_extractor.rb
+++ app/services/schedules_extractor.rb
@@ -1,9 +1,14 @@
+require "http"
+
 class SchedulesExtractor
+  API_URL = 'https://aplicacao7.tst.jus.br/pautaws/rest/pautas'
+
   def initialize()
     # initialize
   end
 
   def call
-    # do stuff
+    response = HTTP.get(API_URL)
+    schedules = response.parse
   end
 end
--- spec/services/schedules_extractor_spec.rb
+++ spec/services/schedules_extractor_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe SchedulesExtractor, type: :model do
   describe '#call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "assign api returns some schedule" do
+      SchedulesExtractor.new.call
+
+      expect(schedules).to be > 0
+    end
   end
 end
