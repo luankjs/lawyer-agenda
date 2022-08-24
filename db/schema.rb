983dc0560b5a580facc3a0e75d54660be3d1f882
Luan Santos <luan@tootz.com.br>
2022-08-24 14:14:13 -0300

    feat: part model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_165650) do
+ActiveRecord::Schema.define(version: 2022_08_24_171219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "parts", force: :cascade do |t|
+    t.integer "code"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "schedules", force: :cascade do |t|
     t.integer "adjudicating_part_code"
