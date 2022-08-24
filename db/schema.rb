0cfba1ea633068268b9debbf1ee7d83a302cdf18
Luan Santos <luan@tootz.com.br>
2022-08-24 14:28:17 -0300

    feat: lawyers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_171558) do
+ActiveRecord::Schema.define(version: 2022_08_24_172755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lawyers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "parts", force: :cascade do |t|
     t.integer "code"
