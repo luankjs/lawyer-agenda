6bf0f9d25da7dad28a5b8de6fbad765767acdac1
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-08-26 03:46:36 -0300

    wip: implements tests to part model validation

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -11,10 +11,27 @@
 require 'rails_helper'
 
 RSpec.describe Part, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a id"
-  it "is not valid without a code"
-  it "is not valid without a name"
-  it "is not valid without a created_at"
-  it "is not valid without a updated_at"
+  it "is valid with valid attributes" do
+    expect(build :part, :standard).to be_valid
+  end
+
+  it "is not valid without a id" do
+    expect(build :part, :without_id).to_not be_valid
+  end
+
+  it "is not valid without a code" do
+    expect(build :part, :without_code).to_not be_valid
+  end
+
+  it "is not valid without a name" do
+    expect(build :part, :without_name).to_not be_valid
+  end
+
+  it "is not valid without a created_at" do
+    expect(build :part, :without_created_at).to_not be_valid
+  end
+
+  it "is not valid without a updated_at" do
+    expect(build :part, :without_updated_at).to_not be_valid
+  end
 end
