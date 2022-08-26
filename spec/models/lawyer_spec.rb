7eb643bcfcbc9aaea5e9b0f51d97e78e79391cb5
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-08-26 03:07:48 -0300

    wip: implements tests to lawyer model validation

--- spec/models/lawyer_spec.rb
+++ spec/models/lawyer_spec.rb
@@ -10,10 +10,27 @@
 require 'rails_helper'
 
 RSpec.describe Lawyer, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a id"
-  it "is not valid without a name"
-  it "is not valid without a created_at"
-  it "is not valid without a updated_at"
-  it "is not valid if its name contains special chars"
+  it "is valid with valid attributes" do
+    expect(build :lawyer, :standard).to be_valid
+  end
+
+  it "is not valid without a id" do
+    expect(build :lawyer, :without_id).to_not be_valid
+  end
+
+  it "is not valid without a name" do
+    expect(build :lawyer, :without_name).to_not be_valid
+  end
+
+  it "is not valid without a created_at" do
+    expect(build :lawyer, :without_created_at).to_not be_valid
+  end
+
+  it "is not valid without a updated_at" do
+    expect(build :lawyer, :without_updated_at).to_not be_valid
+  end
+
+  it "is not valid if its name contains special chars" do
+    expect(build :lawyer, :uncomon_name).to_not be_valid
+  end
 end
