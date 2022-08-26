a177b840d601d72a04da5eec435179ba9de28ecc
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-08-26 03:09:32 -0300

    improvement: lawyer model validation

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -8,5 +8,10 @@
 #  updated_at :datetime         not null
 #
 class Lawyer < ApplicationRecord
+  validates_presence_of :id
+  validates_presence_of :name
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
+  validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
   has_and_belongs_to_many :parts
 end
